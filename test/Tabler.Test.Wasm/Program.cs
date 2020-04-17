94bae63a14097369c522f3e94336563440520327
Johan Boström <johan.bostrom@zingtongroup.com>
2020-04-17 09:46:04 +0200

    Changed Wasm target framework

--- test/Tabler.Test.Wasm/Program.cs
+++ test/Tabler.Test.Wasm/Program.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,7 +13,7 @@ namespace Tabler.Test
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
